ec0ef8ba3df549f6f9e75364fb083217fcc43593
Wahid_Ullah <wahidullahdeveloper.777@gmail.com>
2025-08-06 22:51:11 -0700

    Add files via upload

---
+++ arthmatic_operation.py
@@ -0,0 +1,17 @@
+print(7+8) #addition
+print(7-8) #substracton
+print(7*8) #multiplication
+print(7/8) #division
+print(7%8) #reminder modulus
+print(7//8) #floor division MEAN double division
+print(7**8)
+
+
+a=5
+b=4
+print("this value of ' a + b' = ",a+b)
+print("this value of ' a - b' = ",a-b)
+print("this value of ' a % b' = ",a%b)
+print("this value of ' a / b' = ",a/b)
+print("this value of ' a // b' = ",a//b)
+print("this value of ' a ** b' = ",a**b)  
---
+++ two_varaible_addition.py
@@ -0,0 +1,15 @@
+print ( "Hello")
+print(765)
+print(True)
+print(None)
+
+a=7
+print(a)
+
+b=7
+print(b)
+print(a+b)
+
+c="hello"
+print(c)
+print(a+b)
